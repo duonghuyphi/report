e9c5678aba565758a4a8f321477bd1f5d220143f
Lucas_P <duonghuyphi@gmail.com>
2025-06-06 14:51:23 +0700

    Update ReportApplication.java

--- src/main/java/com/engagePlus/report/ReportApplication.java
+++ src/main/java/com/engagePlus/report/ReportApplication.java
@@ -2,6 +2,7 @@ package com.engagePlus.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.engagePlus.report")
