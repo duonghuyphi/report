319be30981f6eb9bb89e3a8e4a11f64dd71e87d1
Lucas_P <duonghuyphi@gmail.com>
2025-06-11 15:37:50 +0700

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("reportApp", []);
 
 //var API_BASE_URL = "http://localhost:8080";
-var API_BASE_URL = 'https://report-4luc.onrender.com';
+var API_BASE_URL = 'https://report-1pjd.onrender.com';
 
 app.controller("ReportController", function ($scope, $http) {
     $scope.tables = [];
